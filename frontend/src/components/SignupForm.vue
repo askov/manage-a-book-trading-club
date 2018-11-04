0151725f4974e6332a51e1bf5e1b1fb67e8078fe
Alexandr Askov <aleksandr.askov@yandex.ru>
2018-11-04 18:35:08 +0700

    updated vuelidation types, signup form

--- frontend/src/components/SignupForm.vue
+++ frontend/src/components/SignupForm.vue
@@ -1,36 +1,41 @@
 <template>
-  <b-form @submit.prevent="onSubmit" novalidate validated="true" autocomplete="off">
+  <b-form @submit.prevent="onSubmit" novalidate autocomplete="off">
     <b-form-group id="signupFormName" label="Name:" label-for="signupFormName">
       <b-form-input
         id="signupFormName"
         type="text"
+        :class="{ 'is-invalid': $v.form.username.$error }"
         v-model="form.username"
-        required
         placeholder="Enter name"
       ></b-form-input>
+      <b-form-invalid-feedback v-if="!$v.form.username.required">Name is required</b-form-invalid-feedback>
+      <b-form-invalid-feedback
+        v-if="!$v.form.username.minLength"
+      >Name must be at least {{$v.form.username.$params.minLength.min}} characters</b-form-invalid-feedback>
     </b-form-group>
-    <b-form-group
-      id="signupFormEmail"
-      label="Email address:"
-      label-for="signupFormEmail"
-      description="We'll never share your email with anyone else."
-    >
+    <b-form-group id="signupFormEmail" label="Email address:" label-for="signupFormEmail">
       <b-form-input
         id="signupFormEmail"
         type="email"
         v-model="form.email"
-        required
+        :class="{ 'is-invalid': $v.form.email.$error }"
         placeholder="Enter email"
       ></b-form-input>
+      <b-form-invalid-feedback v-if="!$v.form.email.required">Email is required</b-form-invalid-feedback>
+      <b-form-invalid-feedback v-if="!$v.form.email.email">Email must be valid</b-form-invalid-feedback>
     </b-form-group>
     <b-form-group id="signupFormPassword" label="Password:" label-for="signupFormPassword">
       <b-form-input
         id="signupFormPassword"
         type="password"
         v-model="form.password"
-        required
+        :class="{ 'is-invalid': $v.form.password.$error }"
         placeholder="Enter password"
       ></b-form-input>
+      <b-form-invalid-feedback v-if="!$v.form.password.required">Password is required</b-form-invalid-feedback>
+      <b-form-invalid-feedback
+        v-if="!$v.form.password.minLength"
+      >Password must have at least {{ $v.form.password.$params.minLength.min }} letters.</b-form-invalid-feedback>
     </b-form-group>
     <b-form-group
       id="signupFormPasswordConfirm"
@@ -41,9 +46,10 @@
         id="signupFormPasswordConfirm"
         type="password"
         v-model="form.passwordConfirm"
-        required
+        :class="{ 'is-invalid': $v.form.passwordConfirm.$error }"
         placeholder="Repeat password"
       ></b-form-input>
+      <b-form-invalid-feedback v-if="!$v.form.passwordConfirm.sameAs">Passwords must be identical.</b-form-invalid-feedback>
     </b-form-group>
     <b-button type="submit" variant="primary">Submit</b-button>
   </b-form>
@@ -52,7 +58,7 @@
 <script lang="ts">
 import Vue from 'vue';
 import { validationMixin } from 'vuelidate';
-import { required, minLength } from 'vuelidate/lib/validators';
+import { required, minLength, sameAs, email } from 'vuelidate/lib/validators';
 
 interface Event {
   preventDefault: () => void;
@@ -62,42 +68,43 @@ export default Vue.extend({
   name: 'SignupForm',
   data() {
     return {
-      form: {
-        // email: '',
-        // username: '',
-        // password: '',
-        // passwordConfirm: '',
-      },
+      form: {},
     };
   },
   mixins: [validationMixin],
-  validations: {},
-
-  // validations: {
-  //   form: {
-  //     email: {
-  //       required,
-  //     },
-  //     // username: {
-  //     //   required,
-  //     // },
-  //     // password: {
-  //     //   required,
-  //     // },
-  //     // passwordConfirm: {
-  //     //   required,
-  //     // },
-  //   },
-  // },
+  validations: {
+    form: {
+      email: {
+        required,
+        email,
+      },
+      username: {
+        required,
+        minLength: minLength(4),
+      },
+      password: {
+        required,
+        minLength: minLength(8),
+      },
+      passwordConfirm: {
+        sameAsPassword: sameAs('password'),
+      },
+    },
+  },
   methods: {
     onSubmit(evt: Event): void {
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+        return;
+      }
       // alert(JSON.stringify(this.form));
       this.$store
         .dispatch('newUser', this.form)
         .then((user: object) => {
-          // console.log('#new user!');
+          console.log('#new user!');
           // do something after the new user was successfull created
         })
+
         .catch((error: object) => {
           // do something when something goes wrong
         });
@@ -106,6 +113,5 @@ export default Vue.extend({
 });
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 </style>
--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -13,14 +13,9 @@ export default new Vuex.Store({
   },
   actions: {
     newUser({ commit }, user) {
-      //    apiService.new(user).then((res) => {
-      //   // commit('SET_USER', { res.data }); // commit a mutation that changes the store state
-      //   resolve({some: 1});
-      // }).catch(() => reject({ some: 2 }));
       return new Promise((resolve, reject) => {
-
+        resolve({ some: 1 });
         apiService.new(user).then((res) => {
-          // commit('SET_USER', { res.data }); // commit a mutation that changes the store state
           resolve({ some: 1 });
         }).catch(() => reject({ some: 2 }));
       });
