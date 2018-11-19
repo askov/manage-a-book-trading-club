4d022a6aa291d0f28a6db1c25e90356ab10f4518
Alexandr Askov <aleksandr.askov@yandex.ru>
2018-11-19 22:01:50 +0700

    django login url

--- backend/auth/urls.py
+++ backend/auth/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
 
+from rest_framework.authtoken import views as rest_framework_views
 urlpatterns = [
-    url(r'register', views.UserCreate.as_view())
+    url(r'register', views.UserCreate.as_view()),
+    url(r'^me/$', rest_framework_views.obtain_auth_token,
+        name='me')
 ]
