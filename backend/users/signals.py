0716855cdab9c41e8cc6d5a2b9d0b927ff7de167
Alexandr Askov <aleksandr.askov@yandex.ru>
2018-12-04 22:18:44 +0700

    remove unused signal, docstrings

--- backend/users/signals.py
+++ backend/users/signals.py
@@ -1,19 +1,21 @@
 from django.contrib.auth.models import User
 from users.models import Profile
-from django.core.signals import request_finished
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-@receiver(request_finished)
-def my_callback(sender, **kwargs):
-    print("Request finished!")
 
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
+def create_user_profile(_sender, instance, created, **_kwargs):
+    """
+    Creates user profile when user created
+    """
     if created:
         Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
+def save_user_profile(_sender, instance, **_kwargs):
+    """
+    Updates user profile when user saved
+    """
     instance.profile.save()
