885e283581cdc3ed9ee86e011634eb5c76000a96
Alexandr Askov <aleksandr.askov@yandex.ru>
2018-12-25 18:31:36 +0700

    docker config update: volumes for packages

--- backend/create_and_apply_migrations.sh
+++ backend/create_and_apply_migrations.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker exec -it mbtc.backend bash -c 'python manage.py makemigrations'
-docker exec -it mbtc.backend bash -c 'python manage.py migrate'
+docker exec -it mbtc.backend bash -c "python manage.py makemigrations"
+docker exec -it mbtc.backend bash -c "python manage.py migrate"
