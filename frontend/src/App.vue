254310bc9e3d20bc7e37a923e20c95dbdbe2faf4
Alexandr Askov <aleksandr.askov@yandex.ru>
2019-01-14 21:57:59 +0700

    main footer skeleton

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
     <div class="container-fluid">
-      <TopNavbar/>
+      <TopNavbar />
     </div>
-    <transition name="fade" mode="out-in">
-      <router-view/>
+    <transition name="fade"
+                mode="out-in">
+      <router-view />
     </transition>
+    <div class="container-fluid">
+      <Footer />
+    </div>
   </div>
+
+
   <!-- <div id="app" class="container-fluid">
     <TopNavbar/>
     <transition name="fade" mode="out-in">
@@ -17,22 +23,25 @@
 
 
 <script lang="ts">
-import Vue from 'vue';
-import TopNavbar from '@/components/TopNavbar/TopNavbar.vue';
-import user from '@/store/modules/user';
+  import Vue from 'vue';
+  import TopNavbar from '@/components/TopNavbar/TopNavbar.vue';
+  import user from '@/store/modules/user';
+  import Footer from '@/components/Footer/Footer.vue';
 
 
-export default Vue.extend({
-  name: 'appRoot',
-  components: {
-    TopNavbar,
-  },
-  mounted() {
-    if (user.isLoggedIn) {
-      user.dispatchObtainProfile();
-    }
-  },
-});
+  export default Vue.extend({
+    name: 'appRoot',
+    components: {
+      TopNavbar,
+      Footer,
+    },
+    mounted() {
+      if (user.isLoggedIn) {
+        user.dispatchObtainProfile();
+      }
+    },
+  });
+
 </script>
 
 <style lang="scss">
---
+++ frontend/src/components/Footer/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer class="bg-dark text-secondary">
+    created by alex askov {{year}}
+  </footer>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+
+export default Vue.extend({
+  name: 'Footer',
+  data(): {
+    year: number,
+  } {
+    return {
+      year: new Date().getFullYear(),
+    };
+  },
+
+});
+</script>
+
+<style scoped lang="scss">
+</style>
